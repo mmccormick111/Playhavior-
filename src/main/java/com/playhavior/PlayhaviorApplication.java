28a438f9de3dbb482da07b4b0788e6c0c9f05635
Murdock McCormick III <booradley2450@gmail.com>
2026-08-25 19:15:38 -0500

    Scaffold Spring Boot 4.1.1 app (Web, JPA, Thymeleaf, H2, Postgres, Validation, DevTools)

---
+++ src/main/java/com/playhavior/PlayhaviorApplication.java
@@ -0,0 +1,13 @@
+package com.playhavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayhaviorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayhaviorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playhavior/PlayhaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playhavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayhaviorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
